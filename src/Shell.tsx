f89a2200d15e92404ab60c824a008e4626a47cc0
Hassaan Anjum <hassaananjum@gmail.com>
2018-04-02 16:13:48 +0500

    updated style

--- src/Shell.tsx
+++ src/Shell.tsx
@@ -163,8 +163,10 @@ class ShellContainer extends React.Component<Props> implements ShellFunctions {
                     tabIndex={ 0 }
                     type="button"
                 >
-                    <svg>
-                        <path d="M26.79 9.38A0.31 0.31 0 0 0 26.79 8.79L0.41 0.02C0.36 0 0.34 0 0.32 0 0.14 0 0 0.13 0 0.29 0 0.33 0.01 0.37 0.03 0.41L3.44 9.08 0.03 17.76A0.29 0.29 0 0 0 0.01 17.8 0.28 0.28 0 0 0 0.01 17.86C0.01 18.02 0.14 18.16 0.3 18.16A0.3 0.3 0 0 0 0.41 18.14L26.79 9.38ZM0.81 0.79L24.84 8.79 3.98 8.79 0.81 0.79ZM3.98 9.37L24.84 9.37 0.81 17.37 3.98 9.37Z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="20" height="20" viewBox="0 0 20 20" data-tags="send">
+                        <g fill="#444" transform="scale(0.01953125 0.01953125)">
+                            <path d="M0 0l1024 512-1024 512v-1024zM0 409.6v204.8l512-102.4-512-102.4z"/>
+                        </g>
                     </svg>
                 </button>
                 <button
